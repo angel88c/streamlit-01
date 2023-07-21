900703b182cdb1367cf510b2d3ad522ba6a23d34
Angel Carreon Silva <c_angel@MacBook-Air-de-Angel.local>
2023-07-21 14:44:04 -0600

    add load_data

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import pandas as pd
 import streamlit as st
 
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+
 st.title("My app connect to google sheet")
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+st.write(df)
+#for row in df.itertuples():
+#    st.write(f'{row.actividad} {row.horas}')
